179046e549929ca5ba7830d67b15aba4f19d3eaa
Edouard DUPIN <yui.heero@gmail.com>
2014-03-31 16:34:29 +0200

    [DEV] update the ogg and tremor with the new lutin build system

---
+++ lutin_ogg.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+import lutinModule as module
+import lutinTools as tools
+import lutinDebug as debug
+
+def get_desc():
+	return "ogg : Ogg-tremor library for ogg audio decoding"
+
+
+def create(target):
+	myModule = module.Module(__file__, 'ogg', 'LIBRARY')
+	
+	myModule.add_src_file([
+		'ogg/framing.c',
+		'ogg/bitwise.c',
+		'tremor/floor0.c',
+		'tremor/vorbisfile.c',
+		'tremor/synthesis.c',
+		'tremor/res012.c',
+		'tremor/block.c',
+		'tremor/mdct.c',
+		'tremor/sharedbook.c',
+		'tremor/codebook.c',
+		'tremor/floor1.c',
+		'tremor/window.c',
+		'tremor/registry.c',
+		'tremor/info.c',
+		'tremor/mapping0.c'
+		])
+	
+	
+	myModule.add_export_path(tools.get_current_path(__file__))
+	myModule.add_path(tools.get_current_path(__file__)+"/ogg/")
+	myModule.add_path(tools.get_current_path(__file__)+"/tremor/")
+	
+	# add the currrent module at the 
+	return myModule
+
+
+
+
+
+
+
+
+
